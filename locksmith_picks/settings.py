b95579861e9e2b7991da7d3684363b7f28400bc1
pmaddula4 <pranavmaddula36@gmail.com>
2025-06-27 22:13:56 -0500

    update settings and yml

--- locksmith_picks/settings.py
+++ locksmith_picks/settings.py
@@ -88,6 +88,7 @@ USE_TZ = True
 # Static files
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'locksmith_picks_app', 'static')]
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
